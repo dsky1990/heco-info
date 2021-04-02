52bf2dd0c86bd2d00bf35bd39724b1229832ded6
Dsky <dutian1990@gmail.com>
2021-04-02 17:02:31 +0800

    Add files via upload

---
+++ precache-manifest.2b178cc578e3aaa2bf544b2dcc536613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cc97974f81c6430332fc539e1abe15",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/index.html"
+  },
+  {
+    "revision": "62011b17ec9caa8b7e50",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/static/js/2.f3d1e0e8.chunk.js"
+  },
+  {
+    "revision": "627773ea9d4d22328b7e3c164cff9ea5",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/static/js/2.f3d1e0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066b687617021deae67f",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/static/js/main.ec8eb654.chunk.js"
+  },
+  {
+    "revision": "c8d9953f385ee7e6b4e8",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/static/js/runtime-main.cbcf3a04.js"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "https://cdn.jsdelivr.net/gh/dsky1990/info@main/static/media/logo.8e72f6f2.png"
+  }
+]);
